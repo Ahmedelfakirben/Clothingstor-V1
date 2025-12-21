b40c992bd7c396cf498080722d0c2415d26efa2f
Ahmed El Fakir <ahmed@elfakir.com>
2025-12-21 16:39:53 +0100

    Fix: Explicit SW registration in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,18 @@
 import { createRoot } from 'react-dom/client';
+import { registerSW } from 'virtual:pwa-register';
 import App from './App.tsx';
 import './index.css';
+
+// Force SW registration
+const updateSW = registerSW({
+  onNeedRefresh() {
+    console.log('UsePWA: New content available, click on reload button to update.');
+  },
+  onOfflineReady() {
+    console.log('UsePWA: App ready to work offline');
+  },
+  immediate: true
+});
 
 createRoot(document.getElementById('root')!).render(
   <App />
